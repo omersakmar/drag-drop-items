d4f9544dd2b035ecad36f49aea298216793a3a37
Ömer Sakmar <omersakmar@gmail.com>
2022-11-21 20:17:23 +0300

    changed file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
-import { MainBoard } from './components/BoardItem';
+import { MainBoard } from './components/MainBoard';
 
 function App() {
   return (
